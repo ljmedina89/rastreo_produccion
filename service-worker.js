5c21fe99c2172378ddaf5a12ef85acbc57b24f6b
ljmedina89 <leomedinaricachi@gmail.com>
2025-11-02 15:55:30 -0600

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "beloura-cache-v1";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
